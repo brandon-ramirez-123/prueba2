f5c7e10896610c0a55867f06e9e34dd557270fa3
dev777-sudo <bv777bv777@gmail.com>
2021-05-23 11:32:42 -0500

    version 4  verificacion de parametros

--- logica/solECS.py
+++ logica/solECS.py
@@ -10,31 +10,36 @@ class EcuacionSegundoGrado():
     def SolucionECS(self):
         self.x1 = ""
         self.x2 = ""
-        if self.a.isnumeric() and self.b.isnumeric() and self.c.isnumeric():
-            a = int(self.a)
-            b = int(self.b)
-            c = int(self.c)
-            d = b * b - 4 * a * c
-            if d >= 0:
-                r1 = (-b + math.sqrt(d)) / (2 * a)
-                r2 = (-b - math.sqrt(d)) / (2 * a)
-                self.x1 = "{0:.2f}".format(r1)
-                self.x2 = "{0:.2f}".format(r2)
-            else:
-                r1 = (-b) / (2 * a)
-                r2 = (math.sqrt(-d)) / (2 * a)
-                self.x1 = "{0:.2f}".format(r1) + "+" + "{0:.2f}".format(r2) + "i"
-                self.x2 = "{0:.2f}".format(r1) + "-" + "{0:.2f}".format(r2) + "i"
-            return self.x1, self.x2
+        d = self.b * self.b - 4 * self.a * self.c
+        if d >= 0:
+            r1 = (-self.b + math.sqrt(d)) / (2 * self.a)
+            r2 = (-self.b - math.sqrt(d)) / (2 * self.a)
+            self.x1 = "{0:.2f}".format(r1)
+            self.x2 = "{0:.2f}".format(r2)
         else:
-            self.x1 = "ERROR"
-            return self.x1, self.x2
+            r1 = (-self.b) / (2 * self.a)
+            r2 = (math.sqrt(-d)) / (2 * self.a)
+            self.x1 = "{0:.2f}".format(r1) + "+" + "{0:.2f}".format(r2) + "i"
+            self.x2 = "{0:.2f}".format(r1) + "-" + "{0:.2f}".format(r2) + "i"
+        return self.x1, self.x2
 
     def DefinirParametros(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+        self.a = self.ParametroAFloat(a)
+        self.b = self.ParametroAFloat(b)
+        self.c = self.ParametroAFloat(c)
 
     def ImprimirSolucion(self):
+        print("Ecuacion de segundo grado:")
+        print(str(self.a)+"x^2 + "+str(self.b)+"x + "+str(self.c))
+        print("\nSolucion:")
         print("X1 = " + self.x1)
         print("X2 = " + self.x2)
+
+    def ParametroAFloat (self, parametro):
+        if not type(parametro) is float:
+            try:
+                parametro=float(parametro)
+            except ValueError:
+                print(f"El parametro: \"{parametro}\" no se puede converir a float")
+                exit(1)
+        return parametro
