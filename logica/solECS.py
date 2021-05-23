9e0184c206125bd3582f80647247c591ab9c4500
dev777-sudo <bv777bv777@gmail.com>
2021-05-23 11:18:38 -0500

    version 3

--- logica/solECS.py
+++ logica/solECS.py
@@ -21,9 +21,8 @@ class EcuacionSegundoGrado():
                 self.x1 = "{0:.2f}".format(r1)
                 self.x2 = "{0:.2f}".format(r2)
             else:
-                d = d * -1
                 r1 = (-b) / (2 * a)
-                r2 = (math.sqrt(d)) / (2 * a)
+                r2 = (math.sqrt(-d)) / (2 * a)
                 self.x1 = "{0:.2f}".format(r1) + "+" + "{0:.2f}".format(r2) + "i"
                 self.x2 = "{0:.2f}".format(r1) + "-" + "{0:.2f}".format(r2) + "i"
             return self.x1, self.x2
